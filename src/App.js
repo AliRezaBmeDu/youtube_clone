25eb6a3ab3e6878a0ed189c3aa114cdd4f2324ea
AliReza <ali.reza.buet@gmail.com>
2023-04-05 17:17:41 +0600

    index.css added. necessary modules(BrowserRouter, Routes, Route, Box) was imported to app.js file from appropriate modulesets.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Box } from '@mui/material'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+        <Box sx={{
+            backgroundColor: '#000', color:"#fff"
+        }}> <h1>Welcome</h1>
+            This is a navbar
+            <Routes>
+                the routes
+            </Routes>
+        </Box>
+
+        
+    </BrowserRouter>
   )
 }
 
