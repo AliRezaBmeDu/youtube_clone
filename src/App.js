d9d77637f0dc80b4d7bbbd0b4269999c2d274cd1
AliReza <ali.reza.buet@gmail.com>
2023-04-05 17:35:04 +0600

    Necessary components are created in the components folder and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { Box } from '@mui/material'
 
+import {Feed, VideoDetail, ChannelDetail, SearchFeed, Navbar} from './components'
+
 const App = () => {
   return (
     <BrowserRouter>
         <Box sx={{
             backgroundColor: '#000', color:"#fff"
-        }}> <h1>Welcome</h1>
-            This is a navbar
+        }}> 
+            <Navbar />
             <Routes>
-                the routes
+                <Route path = '/' exact element={<Feed />} />
+                <Route path = '/video:id' element={<VideoDetail />} />
+                <Route path = '/channel:id' element={<ChannelDetail />} />
+                <Route path = '/search/searchTerm' element={<SearchFeed />} />
+                
             </Routes>
         </Box>
 
